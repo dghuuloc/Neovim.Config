6cdcf16fb9109dd9ccb780dfea1fd9363c30ed66
dghuuloc <dghuuloc@gmail.com>
2024-08-27 00:37:44 +0700

    adding Windows PowerShell

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,11 +14,11 @@
 
 require('configs')
 
--- vim.cmd("let &shell = has('win32') ? 'powershell' : 'pwsh'")
--- vim.cmd("let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'")
--- vim.cmd("let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'")
--- vim.cmd("let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'")
--- vim.cmd("set shellquote= shellxquote=")
+vim.cmd("let &shell = has('win32') ? 'powershell' : 'pwsh'")
+vim.cmd("let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'")
+vim.cmd("let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'")
+vim.cmd("let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'")
+vim.cmd("set shellquote= shellxquote=")
 
 local set = vim.opt_local
 
