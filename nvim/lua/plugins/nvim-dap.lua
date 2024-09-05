5ef937ef283fcb4eeba3562c5eabd1a47ccb4f8b
dghuuloc <dghuuloc@gmail.com>
2024-09-05 08:04:49 +0700

    something changed 05.09.2024

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -19,12 +19,22 @@ return {
         dap.listeners.before.launch.dapui_config = function()
             dapui.open()
         end
+        dap.listeners.before.attach.dapui_config = function()
+          dapui.open()
+        end
+        -- Automatically close the UI when a new debug session is created.
+        dap.listeners.before.event_terminated.dapui_config = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+        end
 
-        -- set a vim motion for toggling a breakpoint at the line where the cursor is currently on
+        -- set a vim motion to toggle a breakpoint at the line where the cursor is currently on
         -- F9 (Toggle breakpoint): This allows you to set or remove a breakpoint on the current line of code.
         vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "[D]ebug [T]oggle Breakpoint" })
 
-        -- set a vim motion for starting the debugger and launch the debugging ui
+        -- set a vim motion to start the debugger and launch the debugging ui
         -- F8 (Resume execution): This allows you to continue executing the program without stopping debugging.
         vim.keymap.set("n", "<F8>", dap.continue, { desc = "[D]ebug [S]tart" })
 
@@ -40,17 +50,30 @@ return {
         -- set a vim motion to close the debugging ui
         vim.keymap.set("n", "<leader>dc", dapui.close, {desc = "[D]ebug [C]lose"})
 
+        -- set a vim motions to clear all breakpoints
+        vim.keymap.set("n", "<leader>dC", function()
+            dap.clear_breakpoints()
+        end, { desc = "Clear all Breakpoints" })
+
+        -- set a vim motions to close debugger and clear breakpoints
+        vim.keymap.set("n", "<leader>de", function()
+        dap.clear_breakpoints()
+        dapui.toggle({})
+        dap.terminate()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) --
+        end, { desc = "Close debugger and end debugging session" })
+
         -- Add dap configurations based on your language/adapter settings
         dap.configurations = {
             -- configurations for Java languages
-            -- java = {
-            --     {
-            --         type = "java",
-            --         name = "Debug",
-            --         request = "launch",
-            --         program = "${file}",
-            --     },
-            -- },
+            java = {
+                {
+                    type = "java",
+                    name = "Debug",
+                    request = "launch",
+                    program = "${file}",
+                },
+            },
 
             -- configurations for other languages
         }
