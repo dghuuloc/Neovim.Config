6270469704d9e6af0fa205a5e98e5789c1f6043f
dghuuloc <dghuuloc@gmail.com>
2024-09-16 23:42:27 +0700

    adding user command for delRegs

--- nvim/lua/configs/cmds.lua
+++ nvim/lua/configs/cmds.lua
@@ -35,3 +35,17 @@ vim.api.nvim_create_autocmd('DiagnosticChanged', {
   end,
 })
 
+-- Clear Register
+function ClearReg()
+    vim.cmd [[
+        let regs=split('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-"', '\zs')
+        for r in regs
+            call setreg(r, [])
+        endfor
+        ]] 
+end
+
+vim.api.nvim_create_user_command('DelRegs', function()
+  ClearReg()
+end, {})
+
