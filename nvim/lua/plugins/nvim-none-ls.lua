0f519207380ef34437447bfd3ca809263804d854
dghuuloc <dghuuloc@gmail.com>
2025-02-12 17:27:08 +0700

    adding none-ls for formatting

---
+++ nvim/lua/plugins/nvim-none-ls.lua
@@ -0,0 +1,21 @@
+return {
+    "nvimtools/none-ls.nvim",
+
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                -- for formatting Lua
+                null_ls.builtins.formatting.stylua,
+
+                -- for formatting Java
+                null_ls.builtins.formatting.google_java_format,
+
+                -- for formatting xml
+                --null_ls.builtins.formatting.xmlformatter,
+
+            },
+        })
+    end,
+}
